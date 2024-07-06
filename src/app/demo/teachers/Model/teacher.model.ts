06a262c6324c447970f0f2dd29ab190f7fe9593b
Sony <Sony@DESKTOP-GCTV6AD>
2024-07-06 19:05:45 +0530

    New pages added teacher

---
+++ src/app/demo/teachers/Model/teacher.model.ts
@@ -0,0 +1,17 @@
+
+export interface Teachers {
+    Id?: number;
+    fname?: string;
+    Name?: string;
+    lname?: string;
+    email?: string;
+    dob?: Date;
+    Phone?: string;
+    Degree?: string;
+    ContactNo?: string;
+    Proficiency?: string;
+    Address?: string;
+    DateofJoining?: Date;
+}
+
+
