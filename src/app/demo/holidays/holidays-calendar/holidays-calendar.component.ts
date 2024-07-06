9dea25b532bc4ec940c5ae3272fc1de0b4043b04
DotNetCoreProjects <pappusingh954055@gmail.com>
2024-07-07 00:51:43 +0530

    holiday calandar

--- src/app/demo/holidays/holidays-calendar/holidays-calendar.component.ts
+++ src/app/demo/holidays/holidays-calendar/holidays-calendar.component.ts
@@ -1,13 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FullCalendarModule } from '@fullcalendar/angular';
 import { SharedModule } from 'src/app/theme/shared/shared.module';
+
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import { Holidays } from '../holiday.model';
+import { HolidaysService } from '../holidays.service';
+import { title } from 'process';
 
 @Component({
   selector: 'app-holidays-calendar',
   standalone: true,
-  imports: [SharedModule],
+  imports: [SharedModule, FullCalendarModule],
   templateUrl: './holidays-calendar.component.html',
   styleUrl: './holidays-calendar.component.scss'
 })
-export default class HolidaysCalendarComponent {
+export default class HolidaysCalendarComponent implements OnInit {
+  constructor(private svc: HolidaysService) {}
 
+  events: Holidays[] = [];
+
+  arrdata!: any;
+  data!: any;
+  //calendarOptions!: CalendarOptions;
+
+  // events: any = [
+  //   {
+  //     title: 'Republic Day',
+  //     date: '2024-07-12',
+  //     color: '#0000ff',
+  //   },
+  //   {
+  //     title: 'holi',
+  //     date: '2024-07-10',
+  //     color: '#0000FF',
+  //   }
+  // ];
+
+  calendarOptions: CalendarOptions = {
+     plugins: [dayGridPlugin],
+    initialView: 'dayGridMonth',
+    weekends: true,
+    eventClick: this.handleDateClick.bind(this)
+  };
+  handleDateClick(arg: any) {
+    //console.log(arg);
+  }
+  ngOnInit(): void {
+    this.bindCal();
+  }
+  bindCal() {
+    this.svc.GetAll(this.events).subscribe((res: any) => {
+      this.events = res;
+      this.arrdata = this.events;
+      this.data = this.arrdata.map((q: any) => {
+        return { title: q.Title, date: q.HolidayDate ,color:q.color};
+      });
+      console.log(this.events)
+      this.calendarOptions = {
+        //plugins: [dayGridPlugin],
+        //initialView: 'dayGridMonth',
+        //weekends: true,
+        events: this.data
+      };
+    });
+  }
 }
